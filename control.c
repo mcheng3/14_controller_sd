adf21dc5f41200da6f27d8e6047fae784baf3dd3
mcheng3 <michael.cheng65@gmail.com>
2017-12-06 23:37:22 -0500

    Add comments

--- control.c
+++ control.c
@@ -8,11 +8,11 @@
 #define KEY 10982
 
 int main( int argc, char *argv[]){
-	if(argc < 2){
+	if(argc < 2){ //invalid number of args
 		printf("not enough arguments: 1 or more arguments required\n");
 	}
 	else{
-		if(!strcmp(argv[1], "-c") && argc == 3){
+		if(!strcmp(argv[1], "-c") && argc == 3){ //create semaphore and set value
 			//printf("%d\n", semdes);
 			int semdes;
 			if( semdes = semget(KEY, 1, IPC_EXCL | IPC_CREAT | 0644) != -1){
@@ -27,23 +27,21 @@ int main( int argc, char *argv[]){
 			else{
 				printf("semaphore already exists\n");
 			}
-
 		}
-		else if(!strcmp(argv[1], "-v") && argc == 2){
+		else if(!strcmp(argv[1], "-v") && argc == 2){ //View the value of semaphore
 			int semdes = semget(KEY, 0, 0);
 			//printf("semdes: %d\n", semdes);
 			int val = semctl(semdes, 0, GETVAL);
 			printf("semaphore value: %d\n", val);
 		}
-		else if(!strcmp(argv[1], "-r") && argc == 2){
+		else if(!strcmp(argv[1], "-r") && argc == 2){ //Remove the semaphore
 			int semdes = semget(KEY, 0, 0);
 			//printf("semdes: %d\n", semdes);
 			int val = semctl(semdes, 0, IPC_RMID);
 			printf("semaphore removed: %d\n", val);
 		}
-		else{
+		else{ //invalid arguments
 			printf("invalid argument: %s\n", argv[1]);
 		}
-
 	}
 }
